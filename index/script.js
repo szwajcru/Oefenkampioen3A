8e0a4f28b810cfe1f255d66be1556920265f5819
szwajcru <33654718+szwajcru@users.noreply.github.com>
2025-10-11 16:37:59 +0200

    Update script.js

--- index/script.js
+++ index/script.js
@@ -1140,9 +1140,9 @@ function renderResultaatGrafiekOp(canvasId, instanceKey = '_chart') {
               const d = laatste[ctx.dataIndex];
               if (!d) return '';
               const goed = d.goed ?? 0, fout = d.fout ?? 0, totaal = d.totaal ?? 0, ipm = d.ipm ?? 0;
-              const fout = d.fout ?? 0,
-              const totaal = goed+fout;
-              const perc = d.percentage;
+              fout = d.fout ?? 0,
+              totaal = goed+fout;
+              perc = d.percentage;
               return [
                 `Woordjes per minuut: ${ipm}`,
                 `Juist: ${goed}`,
