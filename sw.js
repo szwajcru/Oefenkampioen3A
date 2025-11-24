61258d2351d20255b2438d7a09b782f500a3c714
Ruud Szwajcer <ruud.szwajcer@valcon.com>
2025-11-24 15:34:21 +0100

    poging 9

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — versie-consistente precache + bot-vriendelijk
 // Geen importScripts nodig, versie staat nu hier:
-const SITE_VERSION = '2025-11-24-1520';
+const SITE_VERSION = '2025-11-24-1530';
 const CACHE_NAME = 'site-cache-' + SITE_VERSION;
 
 
